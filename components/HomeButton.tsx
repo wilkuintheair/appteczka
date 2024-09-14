04b35840915352629b307b092472eb337f0d0538
Piotr Wilk <pwilk@bizimply.com>
2024-09-14 23:43:51 +0200

    improve HomeButton

--- components/HomeButton.tsx
+++ components/HomeButton.tsx
@@ -1,12 +1,18 @@
-import { Href, Link } from "expo-router";
+import { Href, useRouter } from "expo-router";
 import { Button } from "@rneui/themed";
+import { ReactNode } from "react";
 
-type Props = { href: Href<string>; children: React.ReactNode };
+type Props = { href: Href<string>; children: ReactNode };
 
 export const HomeButton = ({ href, children }: Props) => {
+  const router = useRouter();
+  const onPress = () => {
+    router.navigate(href);
+  };
+
   return (
-    <Link href={href} asChild>
-      <Button size={"lg"}>{children}</Button>
-    </Link>
+    <Button onPress={onPress} size={"lg"}>
+      {children}
+    </Button>
   );
 };
