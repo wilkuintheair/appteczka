4b654e3ae939f124ed05439efca82dc56244f243
Piotr Wilk <pwilk@bizimply.com>
2024-09-15 10:32:57 +0200

    improve rank screen

--- app/(tabs)/rank.tsx
+++ app/(tabs)/rank.tsx
@@ -2,10 +2,14 @@ import auth from "@react-native-firebase/auth";
 import { GoogleSignin } from "@react-native-google-signin/google-signin";
 import { Button, Icon, makeStyles, useTheme } from "@rneui/themed";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { Image, View } from "react-native";
+import { Text } from "@/components/Text";
 
 export default function RankScreen() {
   const styles = useStyles();
   const { colors } = useTheme().theme;
+
+  const user = auth().currentUser;
 
   async function onGoogleButtonPress() {
     // Check if your device supports Google Play
@@ -35,16 +39,31 @@ export default function RankScreen() {
           color={colors.secondary}
         />
       }
-      headerBackgroundColor={{ dark: "#000000", light: "#FFFFFF" }}
+      headerBackgroundColor={{ dark: colors.grey5, light: colors.grey5 }}
     >
-      <Button
-        title="Google Sign-In"
-        onPress={() =>
-          onGoogleButtonPress().then(() =>
-            console.log("Signed in with Google!"),
-          )
-        }
-      />
+      {!user ? (
+        <Button
+          title="Google Sign-In"
+          onPress={() =>
+            onGoogleButtonPress().then(() =>
+              console.log("Signed in with Google!"),
+            )
+          }
+        />
+      ) : (
+        <View style={styles.container}>
+          <View style={styles.headerContainer}>
+            {user.photoURL ? (
+              <Image
+                source={{ uri: user.photoURL }}
+                style={{ width: 100, height: 100, borderRadius: 50 }}
+              />
+            ) : null}
+            <Text h3>{user.displayName}</Text>
+          </View>
+          <Button title="Sign Out" onPress={() => auth().signOut()} />
+        </View>
+      )}
     </ParallaxScrollView>
   );
 }
@@ -55,6 +74,9 @@ const useStyles = makeStyles(({ spacing }) => ({
   },
   container: {
     flex: 1,
-    padding: spacing.xl,
+  },
+  headerContainer: {
+    alignItems: "center",
+    justifyContent: "center",
   },
 }));
--- components/ParallaxScrollView.tsx
+++ components/ParallaxScrollView.tsx
@@ -1,13 +1,14 @@
-import type { PropsWithChildren, ReactElement } from 'react';
-import { StyleSheet, useColorScheme } from 'react-native';
+import type { PropsWithChildren, ReactElement } from "react";
+import { useColorScheme } from "react-native";
 import Animated, {
   interpolate,
   useAnimatedRef,
   useAnimatedStyle,
   useScrollViewOffset,
-} from 'react-native-reanimated';
+} from "react-native-reanimated";
 
-import { ThemedView } from '@/components/ThemedView';
+import { ThemedView } from "@/components/ThemedView";
+import { makeStyles } from "@rneui/themed";
 
 const HEADER_HEIGHT = 250;
 
@@ -21,7 +22,8 @@ export default function ParallaxScrollView({
   headerImage,
   headerBackgroundColor,
 }: Props) {
-  const colorScheme = useColorScheme() ?? 'light';
+  const styles = useStyles();
+  const colorScheme = useColorScheme() ?? "light";
   const scrollRef = useAnimatedRef<Animated.ScrollView>();
   const scrollOffset = useScrollViewOffset(scrollRef);
 
@@ -32,11 +34,15 @@ export default function ParallaxScrollView({
           translateY: interpolate(
             scrollOffset.value,
             [-HEADER_HEIGHT, 0, HEADER_HEIGHT],
-            [-HEADER_HEIGHT / 2, 0, HEADER_HEIGHT * 0.75]
+            [-HEADER_HEIGHT / 2, 0, HEADER_HEIGHT * 0.75],
           ),
         },
         {
-          scale: interpolate(scrollOffset.value, [-HEADER_HEIGHT, 0, HEADER_HEIGHT], [2, 1, 1]),
+          scale: interpolate(
+            scrollOffset.value,
+            [-HEADER_HEIGHT, 0, HEADER_HEIGHT],
+            [2, 1, 1],
+          ),
         },
       ],
     };
@@ -50,7 +56,8 @@ export default function ParallaxScrollView({
             styles.header,
             { backgroundColor: headerBackgroundColor[colorScheme] },
             headerAnimatedStyle,
-          ]}>
+          ]}
+        >
           {headerImage}
         </Animated.View>
         <ThemedView style={styles.content}>{children}</ThemedView>
@@ -59,18 +66,18 @@ export default function ParallaxScrollView({
   );
 }
 
-const styles = StyleSheet.create({
+const useStyles = makeStyles(({ spacing }) => ({
   container: {
     flex: 1,
   },
   header: {
     height: 250,
-    overflow: 'hidden',
+    overflow: "hidden",
   },
   content: {
     flex: 1,
-    padding: 32,
+    padding: spacing.xl,
     gap: 16,
-    overflow: 'hidden',
+    overflow: "hidden",
   },
-});
+}));
